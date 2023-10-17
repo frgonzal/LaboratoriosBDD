7f2a0cb85be04e93117bc0fb63519d5e6a5600bf
FrancoGonzalezL <franco.gonzalez.l@ug.uchile.cl>
2023-10-16 21:02:56 -0300

    problema

--- Laboratorio_5/lab5.py
+++ Laboratorio_5/lab5.py
@@ -1,9 +1,27 @@
+#############################
+###      INTEGRANTES      ###   
+#############################
+###      Martín Bravo     ###
+###    Franco González    ###
+###      Iván Vidal       ###
+#############################
 import psycopg2
 import psycopg2.extras
 import csv
 import re
 
-nombre_grupo = "superheroes.CBaleRico_"
+nombre_grupo      = "superheroes.CBaleRico_"
+ 
+T_character   = nombre_grupo + "Character"
+T_superheroe  = nombre_grupo + "Superheroe"
+T_relation    = nombre_grupo + "Relation"
+T_work        = nombre_grupo + "WorkOcupation"
+T_alterego    = nombre_grupo + "Alterego"
+
+T_related_to  = nombre_grupo + "related_to"
+T_haswork     = nombre_grupo + "hasWork"
+T_hasalterego = nombre_grupo + "hasAlterego"
+
 
 def replace_inside_parentheses(match):
     return match.group(0).replace(" ", "-")
@@ -11,14 +29,7 @@ def replace_inside_parentheses(match):
 def del_inside_parentheses(s):
     return re.sub(r"\([^)]*\)","", s).strip()
 
-cur = None
-def findOrInsert(table, name):
-    cur.execute("select id from "+ nombre_grupo+table +" where name=%s limit 1", [name])
-    r = cur.fetchone()
-    if(r):
-        return r[0]
-    cur.execute("insert into "+ nombre_grupo+table +" (name) values (%s) returning id", [name])
-    return cur.fetchone()[0]
+
 
 conn = psycopg2.connect (
     host     = "cc3201.dcc.uchile.cl",
@@ -28,6 +39,68 @@ conn = psycopg2.connect (
     port     = "5440"
 )
 cur = conn.cursor()
+
+def findOrInsert(table, name):
+    cur.execute("select id from "+ table +" where name=%s limit 1", [name])
+    r = cur.fetchone()
+    if(r):
+        return r[0]
+    cur.execute("insert into "+ table +" (name) values (%s) returning id", [name])
+    return cur.fetchone()[0]
+
+def findOrInsertCharacter(name_related):
+    #Buscar en characters
+    cur.execute("select id from "+T_character +" where name=%s limit 1", [name_related])
+    id_character = cur.fetchone()
+    # Buscar en superheroes
+    if not id_character:
+        cur.execute("select id_character from "+T_superheroe+" where name=%s limit 1", [name_related])
+        id_character = cur.fetchone()
+    if not id_character:
+        cur.execute("insert into "+T_character+" (name) values (%s) returning id", [name_related])
+        id_character = cur.fetchone()
+    return id_character[0]
+
+def insertSuperheroe(id_char, name_super, intelligence, strength, speed):
+    cur.execute("select id_character from "+T_superheroe+" where id_character=%s and name=%s", [id_char, name_super])
+    id_super = cur.fetchone()
+    if not id_super:
+        cur.execute("insert into "+ T_superheroe
+                    +" (id_character, name, intelligence, strength, speed)"
+                    +" values (%s, %s, %s, %s, %s)",
+                    [id_char, name_super, intelligence, strength, speed])
+
+def insertHasWork(id_work, id_char):
+    cur.execute("select id_workocupation from "+ T_haswork
+                +" where id_workocupation=%s and id_superheroe=%s limit 1",
+                [id_work, id_char])
+    r = cur.fetchone()
+    if not r:
+        cur.execute("insert into "+ T_haswork
+                    +" (id_workocupation, id_superheroe) values (%s, %s)",
+                    [id_work, id_char])
+
+
+def insertHasAlter(id_alterego, id_char): 
+    cur.execute("select id_alterego from "+ T_hasalterego
+                +" where id_alterego=%s and id_superheroe=%s limit 1",
+                [id_alterego, id_char])
+    r = cur.fetchone()
+    if not r:
+        cur.execute("insert into "+ T_hasalterego
+                    +" (id_alterego, id_superheroe) values (%s, %s)",
+                    [id_alterego, id_char])
+
+def insertHasRelation(id_relation, id_character, id_superheroe):
+    cur.execute("select id_relation from " + T_related_to
+                +" where id_relation=%s and id_character=%s and id_superheroe=%s limit 1",
+                [id_relation, id_character, id_superheroe])
+    r = cur.fetchone()
+    if not r:
+        cur.execute("insert into " + T_related_to
+                    +" (id_relation, id_character, id_superheroe) values (%s, %s, %s)",
+                    [id_relation, id_character, id_superheroe])
+
 
 with open("data.csv","r") as csvfile:
     reader = csv.reader(csvfile, delimiter=',', quotechar='"')
@@ -47,16 +120,16 @@ with open("data.csv","r") as csvfile:
         
         # Nombre completo de personaje
         if row[index_name_char] and row[index_name_char] != "-":
-            name = row[index_name_char].strip()
+            name = row[index_name_char]
         else:
-            name = row[index_name_superh].strip()
+            name = row[index_name_superh]
         if "/" in name:
             name = name.split("/")[0].strip()
         if "(" in name:
             name = del_inside_parentheses(name) # elimina el interior del parentesis
 
         # Nombre superheroe
-        name_super   = row[index_name_superh] 
+        name_super   = row[index_name_superh]
 
         # Atributos
         intelligence = row[index_intelligence] if row[index_intelligence]!="null" else None
@@ -83,36 +156,24 @@ with open("data.csv","r") as csvfile:
         ###################################
 
         ## Character
-        cur.execute("insert into "+nombre_grupo+"Character (name) values (%s) returning id", [name])
-        id_char = cur.fetchone()[0]
+        id_char = findOrInsert(T_character, name)
 
         ## Superheroe
-        cur.execute("insert into "+nombre_grupo+"Superheroe (id_character, name, intelligence, strength, speed) values (%s, %s, %s, %s, %s)",
-                    [id_char, name_super, intelligence, strength, speed])
+        insertSuperheroe(id_char, name_super, intelligence, strength, speed)
 
-        
         ## WorkOcupation
         if works:
             for work in works:
-                # has 
-                id_work = findOrInsert("WorkOcupation", work)
-
-                cur.execute("select id_workocupation from "+nombre_grupo+"hasWork" +
-                            " where id_workocupation=%s and id_superheroe=%s limit 1",
-                            [id_work, id_char])
-                r = cur.fetchone()
-                if not r:
-                    cur.execute("insert into "+nombre_grupo+"hasWork"+
-                                "(id_workocupation, id_superheroe) values (%s, %s)",
-                                [id_work, id_char])
+                id_work = findOrInsert(T_work, work)
+                #has
+                insertHasWork(id_work, id_char)
 
         # Alterego
         if alter_egos:
             for alter_ego in alter_egos:
-                id_alterego = findOrInsert("Alterego", alter_ego)
-                cur.execute("insert into "+nombre_grupo+"hasAlterego"+
-                            " (id_alterego, id_superheroe) values (%s, %s)",
-                            [id_alterego, id_char])
+                id_alterego = findOrInsert(T_alterego, alter_ego)
+                #has
+                insertHasAlter(id_alterego, id_char)
 
 
 # Leer de nuevo para las relaciones
@@ -125,82 +186,80 @@ with open("data.csv","r") as csvfile:
             index_name_superh = row.index("name")
             index_name_char   = row.index("biography__full-name")
             index_relation    = row.index("connections__relatives")
-            continue ##jump to next iteration
+            continue
 
         # Full Name
         if row[index_name_char] and row[index_name_char] != "-":
-            name = row[index_name_char].strip()
+            name = row[index_name_char]
         else:
-            name = row[index_name_superh].strip()
+            name = row[index_name_superh]
         if "/" in name:
             name = name.split("/")[0].strip()
         if "(" in name:
             name = del_inside_parentheses(name) # elimina el interior del parentesis
 
+        #Relations
         relations = row[index_relation] if row[index_relation]!="-" else None
-        if relations:
-            relations = (relations
-                            .replace(";",",") .replace(", ",",")  .replace('\n',"")
-                            .replace(" )",")").replace("( ","(")  .replace("'","")
-                            .replace('"',"")  .replace(") ,","),").replace('\\n',"")
-                            .replace(" ,",",")
-                        )
-            relations = re.sub(r'\([^)]*\)', replace_inside_parentheses, relations)
-            relations = relations.split("),")
+        if not relations: continue
 
-            for relation in relations:
-                if relation and (relation.count("(")!=relation.count(")")):
-                    relation += ")"
-                relation = relation.strip()
-                relation = re.search("([^(]+)[ ]*\(([^) ]+)\)", relation)
+        relations = (relations
+                        .replace(";",",") .replace(", ",",")  .replace('\n',"")
+                        .replace(" )",")").replace("( ","(")  .replace("'","")
+                        .replace('"',"")  .replace(") ,","),").replace('\\n',"")
+                        .replace(" ,",",")
+                    )
+        relations = re.sub(r'\([^)]*\)', replace_inside_parentheses, relations)
+        relations = relations.split("),")
 
-                if relation:
-                    names_related = relation.group(1)
-                    names_related = re.split(r"[,/]", names_related)
-                    names_related = [x.strip() for x in names_related]
-                    names_related = list(filter(lambda x:
+        for relation in relations:
+            if relation and (relation.count("(")!=relation.count(")")):
+                relation += ")"
+            relation = relation.strip()
+            relation = re.search("([^(]+)[ ]*\(([^) ]+)\)", relation)
+
+            if relation:
+                names_related = relation.group(1)
+                names_related = re.split(r"[,/]", names_related)
+                names_related = [x.strip() for x in names_related]
+                names_related = list(filter(lambda x:
+                                            x and\
+                                            x != "Jr." and x != "Sr." and\
+                                            "deceased" not in x.lower() and \
+                                            "unnamed" not in x.lower() and\
+                                            "unidentified" not in x.lower() and\
+                                            x[0].isupper() #nombres comienzan con mayus
+                                    , names_related))
+
+                relations_type = relation.group(2)
+                relations_type = re.split(r"[,/]", relations_type)
+                relations_type = [x.replace("(","").replace(")","").strip() for x in relations_type]
+                relations_type = list(filter(lambda x:
                                                 x and\
-                                                x != "Jr." and x != "Sr." and\
-                                                "deceased" not in x.lower() and \
-                                                "unnamed" not in x.lower() and\
-                                                "unidentified" not in x.lower() and\
-                                                x[0].isupper() #nombres comienzan con mayus
-                                        , names_related))
+                                                x.lower()!="deceased" and \
+                                                x.lower()!="status-unknown" and\
+                                                "relatives" not in x.lower() and\
+                                                x.lower()!="unconfirmed" and\
+                                                x[0].islower() #relaciones comienzan con minus
+                                        , relations_type))
 
-                    relations_type = relation.group(2)
-                    relations_type = re.split(r"[,/]", relations_type)
-                    relations_type = [x.replace("(","").replace(")","").strip() for x in relations_type]
-                    relations_type = list(filter(lambda x:
-                                                    x and\
-                                                    x.lower()!="deceased" and \
-                                                    x.lower()!="status-unknown" and\
-                                                    "relatives" not in x.lower() and\
-                                                    x.lower()!="unconfirmed" and\
-                                                    x[0].islower() #relaciones comienzan con minus
-                                            , relations_type))
+                if names_related and relations_type:
+                    for relation_type in relations_type:
+                        if relations_type[-1] == "s": # si esta en plural
+                            relation_type = relations_type[:-1]
+                        for name_related in names_related:
+                            ##########################
+                            ##      Relaciones      ##
+                            ##########################
 
-                    if names_related and relations_type:
-                        for relation_type in relations_type:
-                            if relations_type[-1] == "s": # si esta en plural
-                                relation_type = relations_type[:-1]
-                            for name_related in names_related:
-                                ##########################
-                                ##      Relaciones      ##
-                                ##########################
+                            # id relation
+                            id_relation   = findOrInsert(T_relation, relation_type)
+                            # id superheroe
+                            id_superheroe = findOrInsert(T_character, name)
+                            # id character
+                            id_character = findOrInsertCharacter(name_related)
 
-                                # id super
-                                id_relation   = findOrInsert("Relation",   relation_type)
-                                id_superheroe = findOrInsert("Character",  name)
-                                id_character  = findOrInsert("Character",  name_related)
-
-                                cur.execute("select id_relation from "+nombre_grupo+"related_to"+
-                                            " where id_relation=%s and id_character=%s and id_superheroe=%s limit 1",
-                                            [id_relation, id_character, id_superheroe])
-                                r = cur.fetchone()
-                                if not r:
-                                    cur.execute("insert into "+nombre_grupo+"related_to"+
-                                                " (id_relation, id_character, id_superheroe) values (%s, %s, %s)",
-                                                [id_relation, id_character, id_superheroe])
+                            # insert relation
+                            insertHasRelation(id_relation, id_character, id_superheroe)
 
 conn.commit()
 conn.close()
